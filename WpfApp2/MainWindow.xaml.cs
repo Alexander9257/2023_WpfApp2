b567f3c9039e5c6094b7ae1d59f35a6ac0bcea56
政安 陳 <4b160067@office.stust.edu.tw>
2023-10-15 17:51:18 +0800

    飲料點餐系統第一版

--- WpfApp2/MainWindow.xaml.cs
+++ WpfApp2/MainWindow.xaml.cs
@@ -28,24 +28,65 @@ namespace WpfApp2
             myDrinks.Add("咖啡小杯", 40);
         }
 
-        private void PlaceOrder(object sender, System.Windows.Controls.TextChangedEventArgs e)
+        private void PlaceOrder(object sender, TextChangedEventArgs e)
         {
             var targetTextBox = sender as TextBox;
 
             bool success = int.TryParse(targetTextBox.Text, out int amount);
 
             if (!success) MessageBox.Show("請輸入整數", "輸入錯誤");
-            else if (amount > 0) MessageBox.Show("請輸入正整數", "輸入錯誤");
+            else if (amount <= 0) MessageBox.Show("請輸入正整數", "輸入錯誤");
             else
             {
-                StackPanel targetstackPanel = targetTextBox.Parent as StackPanel;
-               
-                Label targetLabel = targetstackPanel.Children[0] as Label;
+                var targetstackPanel = targetTextBox.Parent as StackPanel;
+                var targetLabel = targetstackPanel.Children[0] as Label;
                 string drinkName = targetLabel.Content.ToString();
 
                 if (order.ContainsKey(drinkName)) order.Remove(drinkName);
                 order.Add(drinkName, amount);
             }
+        }
+
+        private void OrderButton_Click(object sender, RoutedEventArgs e)
+        {
+            double total = 0.0;
+            double sellPrice = 0.0;
+            string discountMessage = " ";
+            string displayMessage = "訂購清單如下：\n";
+            
+            foreach (var item in order) 
+            {
+                string drinkName = item.Key;
+                int quantity = order[drinkName];
+                int price = drinks[drinkName];
+
+                total+= quantity * price;
+                displayMessage += $"{drinkName} X {quantity}杯，每杯{price}元，總共{price * quantity}元\n";
+            }
+
+            if (total > 500)
+            {
+                discountMessage = "訂購滿500元以上者打8折";
+                sellPrice = total * 0.8;
+            }
+            else if (total > 300)
+            {
+                discountMessage = "訂購滿300元以上者打85折";
+                sellPrice = total * 0.85;
+            }
+            else if (total > 200)
+            {
+                discountMessage = "訂購滿200元以上者打9折";
+                sellPrice = total * 0.9;
+            }
+            else
+            {
+                discountMessage = "訂購未滿200元以上者不打折";
+                sellPrice = total;
+            }
+
+            displayMessage += $"本次訂購總共{order.Count}項，總共{total}元，{discountMessage}，售價{sellPrice}元。\n";
+            TextBlock1.Text = displayMessage;
         }
     }
 }
