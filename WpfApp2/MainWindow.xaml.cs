b04201a74021c59eb5fee34fbf4712bae2a7fc5d
政安 陳 <4b160067@office.stust.edu.tw>
2023-10-15 16:09:40 +0800

    新增drinks,order兩個Dictionary資料結構，建立飲料訂購視窗介面。

--- WpfApp2/MainWindow.xaml.cs
+++ WpfApp2/MainWindow.xaml.cs
@@ -1,17 +1,7 @@
-﻿using System;
+﻿using System.Windows;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
+using System;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace WpfApp2
 {
@@ -20,9 +10,42 @@ namespace WpfApp2
     /// </summary>
     public partial class MainWindow : Window
     {
+        Dictionary<string, int> drinks = new Dictionary<string, int>();
+        Dictionary<string , int> order = new Dictionary<string , int>();
         public MainWindow()
         {
             InitializeComponent();
+            AddNewDrink(drinks);
+        }
+
+        private void AddNewDrink(Dictionary<string, int> myDrinks)
+        {
+            myDrinks.Add("紅茶大杯", 60);
+            myDrinks.Add("紅茶小杯", 40);
+            myDrinks.Add("綠茶大杯", 60);
+            myDrinks.Add("綠茶小杯", 40);
+            myDrinks.Add("咖啡大杯", 80);
+            myDrinks.Add("咖啡小杯", 40);
+        }
+
+        private void PlaceOrder(object sender, System.Windows.Controls.TextChangedEventArgs e)
+        {
+            var targetTextBox = sender as TextBox;
+
+            bool success = int.TryParse(targetTextBox.Text, out int amount);
+
+            if (!success) MessageBox.Show("請輸入整數", "輸入錯誤");
+            else if (amount > 0) MessageBox.Show("請輸入正整數", "輸入錯誤");
+            else
+            {
+                StackPanel targetstackPanel = targetTextBox.Parent as StackPanel;
+               
+                Label targetLabel = targetstackPanel.Children[0] as Label;
+                string drinkName = targetLabel.Content.ToString();
+
+                if (order.ContainsKey(drinkName)) order.Remove(drinkName);
+                order.Add(drinkName, amount);
+            }
         }
     }
 }
